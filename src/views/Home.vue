dddd36755b8b2fd053ee049d132985a985a32c04
mdimran1409036 <mdimran1409036@gmail.com>
2021-01-30 03:24:19 +0600

    Getting real time playlist data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
     <p>Homepage</p>
-    
+    <div v-if="documents"> 
+      <div v-for="doc in documents" :key="doc.id">
+        <h2>{{ doc.title }}</h2> 
+      </div>
+    </div>
+    <div class="error" v-if="error">Could not fetch the data</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+// getting data from firebase using get collection
 
+//  -get realtime data from the playlists collection
+//  -output 2 conditional items -error, or playlist titles
+import getCollection from '@/composables/getCollection'
 
 export default {
   name: 'Home',
   components: {
   
+  },
+  setup() {
+    
+    const{ error, documents} = getCollection('Playlists') 
+    
+
+    return {error, documents}
   }
 }
 </script>
